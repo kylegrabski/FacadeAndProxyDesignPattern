b68f71f0c763243f5c9b1e4d1f6c3058d92bbf4c
Kyle Grabski <76755031+kylegrabski@users.noreply.github.com>
2024-03-17 16:39:07 -0700

    user gateway interface

---
+++ Common/Interfaces/Gateways/IUserApiGateway.cs
@@ -0,0 +1,9 @@
+﻿using FacadeAndProxyDesignPattern.Common.Dto.Responses;
+
+namespace FacadeAndProxyDesignPattern.Common.Interfaces.Gateways;
+
+public interface IUserApiGateway
+{
+    public Task<UserDataResponseDto?> GetUserPlaceholderData(string userId);
+    public Task<IEnumerable<UserPostsResponseDto>> GetUserPlaceholderPosts(string userId);
+}
