3baadf6f9db9b81298acc1816bb3afcb53033a4f
Kyle Grabski <76755031+kylegrabski@users.noreply.github.com>
2024-03-17 16:37:54 -0700

    add json properties

--- Common/Documents/AdminDocument.cs
+++ Common/Documents/AdminDocument.cs
@@ -1,8 +1,12 @@
-﻿namespace FacadeAndProxyDesignPattern.Common.Documents;
+﻿using System.Text.Json.Serialization;
+
+namespace FacadeAndProxyDesignPattern.Common.Documents;
 
 public class AdminDocument
 {
+    [JsonPropertyName("userName")]
     public string UserName { get; set; }
     
+    [JsonPropertyName("isAdmin")]
     public bool IsAdmin { get; set; }
 }
