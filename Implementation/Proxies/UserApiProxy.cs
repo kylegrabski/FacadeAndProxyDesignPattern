ea0eca7fb54adfe02c451fbf6c237073e64638cb
Kyle Grabski <76755031+kylegrabski@users.noreply.github.com>
2024-03-17 16:42:51 -0700

    update return types, WIP LINQ

--- Implementation/Proxies/UserApiProxy.cs
+++ Implementation/Proxies/UserApiProxy.cs
@@ -1,4 +1,6 @@
-﻿using FacadeAndProxyDesignPattern.Common.Dto.Responses;
+﻿using FacadeAndProxyDesignPattern.Common.Domain.Entities;
+using FacadeAndProxyDesignPattern.Common.Dto.Responses;
+using FacadeAndProxyDesignPattern.Common.Interfaces.Gateways;
 using FacadeAndProxyDesignPattern.Common.Interfaces.Proxies;
 using FacadeAndProxyDesignPattern.Common.Interfaces.Repositories;
 
@@ -9,18 +11,38 @@ public class UserApiProxy : IUserApiProxy
     
     private readonly ILogger _logger;
     private readonly IAdminDatabaseRepository _adminDatabaseRepository;
+    private readonly IUserApiGateway _userApiGateway;
 
-    public UserApiProxy(ILogger logger, IAdminDatabaseRepository adminDatabaseRepository)
+    public UserApiProxy(ILogger logger, IAdminDatabaseRepository adminDatabaseRepository, IUserApiGateway userApiGateway)
     {
         _logger = logger;
         _adminDatabaseRepository = adminDatabaseRepository;
+        _userApiGateway = userApiGateway;
     }
     
-    // If the username doesn't exist in the database, isAdmin: false
-    public Task<IEnumerable<UserDataResponseDto>> RequestUserData(string requester)
+    public async Task<(UserDataResponseDto?, IEnumerable<UserPostsResponseDto>?)> RequestUserData(string requester, string userId)
     {
-        var isAdmin = _adminDatabaseRepository.GetUserFromDatabase(requester);
+        var adminDocument = _adminDatabaseRepository.GetRequesterFromDatabase(requester);
+
+        var userData = await _userApiGateway.GetUserPlaceholderData(userId);
+        IEnumerable<UserPostsResponseDto>? userPosts = null;
+        // IEnumerable<UserPostsResponseDto> userPosts = new List<UserPostsResponseDto>();
+
+        // @TODO THIS SHOULD ACTUALLY BE IN THE SERVICE!
+        // userEntities = userData.Select(user =>
+        // {
+        //     var userDataEntity = new UserDataEntity()
+        //     {
+        //         UserData = user
+        //     };
+        //     return userDataEntity;
+        // }).ToList();
         
-        return null;
+        if (adminDocument.IsAdmin)
+        {
+            userPosts = await _userApiGateway.GetUserPlaceholderPosts(userId);
+        }
+        // Check if isAdmin, if so then get user data and user posts, if not then just user data
+        return (userData, userPosts);
     }
 }
